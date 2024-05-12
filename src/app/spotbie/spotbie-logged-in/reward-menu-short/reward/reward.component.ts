6627afef71759be5fc5f0d0f45321c0831a3bb78
0456franco <franco.petitfour001@gmail.com>
2024-05-12 00:55:19 -0400

    ADDED: reward component to display in short reward menu

---
+++ src/app/spotbie/spotbie-logged-in/reward-menu-short/reward/reward.component.ts
@@ -0,0 +1,46 @@
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import {Reward} from '../../../../models/reward';
+import {Preferences} from "@capacitor/preferences";
+
+@Component({
+  selector: 'app-reward',
+  templateUrl: './reward.component.html',
+  styleUrls: ['./reward.component.css'],
+})
+export class RewardComponent implements OnInit, AfterViewInit {
+  @Output() closeWindowEvt = new EventEmitter();
+
+  @Input() fullScreenMode = true;
+  @Input() reward: Reward;
+  @Input() userPointToDollarRatio: number;
+
+  loading = false;
+  successful_url_copy = false;
+  isLoggedIn: string;
+
+  constructor() {}
+
+  async ngAfterViewInit() {
+    const isLoggedInRet = await Preferences.get({key: 'spotbie_loggedIn'});
+    this.isLoggedIn = isLoggedInRet.value;
+
+    // I'm sure there's a better way to do this but I don't have time right now.
+    const closeButton = document.getElementById('sb-closeButtonReward');
+
+    const p =
+      this.isLoggedIn === '0' || !this.isLoggedIn
+        ? document.getElementById('ionToolbarLoggedOut').offsetHeight
+        : document.getElementById('ionToolbarLoggedIn').offsetHeight;
+
+    closeButton.style.top = p + 'px';
+  }
+
+  ngOnInit() {}
+}
