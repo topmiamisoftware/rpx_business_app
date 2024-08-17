4fb7b75ef2372ab7a691d4fcacaf71d38efbf127
0456franco <franco.petitfour001@gmail.com>
2024-08-16 22:18:26 -0400

    update en

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,25 +1,27 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const ngrok = 'https://836f-2601-586-cd00-7900-2586-ce6b-c6a7-c35a.ngrok-free.app/';
-// const ngrok = 'https://api.spotbie.com/';
-const baseUrl = 'https://spotbie.com/';
+
+const ngrok = "https://api.spotbie.com/";
+//const ngrok = 'https://84b6-2601-586-cd00-7900-1c0c-9355-ad1b-66a3.ngrok-free.app/';
+const baseUrl = "https://spotbie.com/";
 
 export const environment = {
   production: true,
   staging: false,
   baseUrl,
-  googleMapsApiKey: 'AIzaSyBg9GGAv2rRn8WQbylRbpF4j6u-9TFxBG8',
-  googlePlacesApiAkey: 'AIzaSyChSn9IE6Dp0Jv8TS013np1b4X1rCsQt_E',
-  mapId: 'IDe50e7edb94949c5',
-  qrCodeLoyaltyPointsScanBaseUrl: baseUrl + 'loyalty-points',
-  qrCodeRewardScanBaseUrl: baseUrl + 'reward',
+  googleMapsApiKey: "AIzaSyBg9GGAv2rRn8WQbylRbpF4j6u-9TFxBG8",
+  googlePlacesApiAkey: "AIzaSyChSn9IE6Dp0Jv8TS013np1b4X1rCsQt_E",
+  mapId: "IDe50e7edb94949c5",
+  qrCodeLoyaltyPointsScanBaseUrl: baseUrl + "loyalty-points",
+  qrCodeRewardScanBaseUrl: baseUrl + "reward",
   publishableStripeKey:
-    'pk_test_51JrUwnGFcsifY4UhCCJp023Q1dWwv5AabBTsMDwiJ7RycEVLyP1EBpwbXRsfn07qpw5lovv9CGfvfhQ82Zt3Be8U00aH3hD9pj',
+    "pk_test_51JrUwnGFcsifY4UhCCJp023Q1dWwv5AabBTsMDwiJ7RycEVLyP1EBpwbXRsfn07qpw5lovv9CGfvfhQ82Zt3Be8U00aH3hD9pj",
   apiEndpoint: `${ngrok}api/`,
   fakeLocation: false,
   myLocX: 25.786286,
   myLocY: -80.186562,
+  installedVersion: '2'
 };
 /*
  * For easier debugging in development mode, you can import the following file
@@ -29,4 +31,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 
-import 'zone.js/dist/zone-error'; // Included with Angular CLI.
+import "zone.js/dist/zone-error"; // Included with Angular CLI.
