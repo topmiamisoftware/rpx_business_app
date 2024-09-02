1ec19c088c78142a6889eb41c4e9cdd697f69daf
0456franco <franco.petitfour001@gmail.com>
2024-09-02 01:46:57 -0400

    REMOVED: env.prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,25 +4,27 @@
 
 const ngrok = "https://4249-2601-586-cd00-7900-942a-3054-dfb4-a324.ngrok-free.app/";
 // const ngrok = "https://api-demo.spotbie.com/";
-// const baseUrl = "https://business-demo.spotbie.com/";
+// const ngrok = "https://api.spotbie.com/";
+
 const baseUrl = "http://localhost:55233/";
+// const baseUrl = "https://business-demo.spotbie.com/";
+// const baseUrl = "https://spotbie.com/";
 
 export const environment = {
   production: true,
   staging: false,
   baseUrl,
-  googleMapsApiKey: "AIzaSyBg9GGAv2rRn8WQbylRbpF4j6u-9TFxBG8",
-  googlePlacesApiAkey: "AIzaSyChSn9IE6Dp0Jv8TS013np1b4X1rCsQt_E",
-  mapId: "IDe50e7edb94949c5",
+  googleMapsApiKey: "",
+  googlePlacesApiAkey: "",
+  mapId: "",
   qrCodeLoyaltyPointsScanBaseUrl: baseUrl + "loyalty-points",
   qrCodeRewardScanBaseUrl: baseUrl + "reward",
-  publishableStripeKey:
-    "pk_test_51JrUwnGFcsifY4UhCCJp023Q1dWwv5AabBTsMDwiJ7RycEVLyP1EBpwbXRsfn07qpw5lovv9CGfvfhQ82Zt3Be8U00aH3hD9pj",
+  publishableStripeKey: "",
   apiEndpoint: `${ngrok}api/`,
   fakeLocation: true,
-  myLocX: 25.786286,
-  myLocY: -80.186562,
-  installedVersion: '3'
+  myLocX: null,
+  myLocY: null,
+  installedVersion: ''
 };
 /*
  * For easier debugging in development mode, you can import the following file
